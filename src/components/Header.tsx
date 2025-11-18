ee52fa64497f0193930d4b635cf0765354989d4d
Linden Holt-Whittaker <36598942+LindenHolt-Whittaker@users.noreply.github.com>
2025-11-18 23:54:17 +0000

    Extend header width to full size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,41 +10,43 @@ const Header = () => {
 
   return (
     <div className="Header">
-      <div className="Header__linkGroup">
-        <NavLink to="/" className="Header__link">
-          {({ isActive }) => (
-            <span className={isActive ? "active" : ""}>Home</span>
-          )}
-        </NavLink>
-        <NavLink to="/onboarding" className="Header__link">
-          {({ isActive }) => (
-            <span className={isActive ? "active" : ""}>Get Schedule</span>
-          )}
-        </NavLink>
-      </div>
+      <div className="Header__container">
+        <div className="Header__linkGroup">
+          <NavLink to="/" className="Header__link">
+            {({ isActive }) => (
+              <span className={isActive ? "active" : ""}>Home</span>
+            )}
+          </NavLink>
+          <NavLink to="/onboarding" className="Header__link">
+            {({ isActive }) => (
+              <span className={isActive ? "active" : ""}>Get Schedule</span>
+            )}
+          </NavLink>
+        </div>
 
-      {!isHomePage && (
-        <form
-          onSubmit={onKeySearch}
-          className={`Header__scheduleKey ${
-            scheduleKey ? "Header__scheduleKey--hasInput" : ""
-          }`}
-        >
-          <div className="Header__scheduleKeyInput">
-            <span className="Header__scheduleKeyHash">#</span>
-            <input
-              type="text"
-              name="scheduleKey"
-              placeholder="Enter schedule key"
-              value={scheduleKey}
-              onChange={(e) => setScheduleKey(e.target.value)}
-            />
-          </div>
-          <button className="Header__scheduleKeyButton" type="submit">
-            ➜
-          </button>
-        </form>
-      )}
+        {!isHomePage && (
+          <form
+            onSubmit={onKeySearch}
+            className={`Header__scheduleKey ${
+              scheduleKey ? "Header__scheduleKey--hasInput" : ""
+            }`}
+          >
+            <div className="Header__scheduleKeyInput">
+              <span className="Header__scheduleKeyHash">#</span>
+              <input
+                type="text"
+                name="scheduleKey"
+                placeholder="Enter schedule key"
+                value={scheduleKey}
+                onChange={(e) => setScheduleKey(e.target.value)}
+              />
+            </div>
+            <button className="Header__scheduleKeyButton" type="submit">
+              ➜
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
