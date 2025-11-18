15b3e34596fc35bab8502a2bb967e2d52592c015
Linden Holt-Whittaker <36598942+LindenHolt-Whittaker@users.noreply.github.com>
2025-11-18 10:57:18 +0000

    Update FieldError component to smoothly transition between error states

--- src/components/FieldError.tsx
+++ src/components/FieldError.tsx
@@ -1,12 +1,46 @@
-import './FieldError.scss';
+import { useEffect, useState } from "react";
+import "./FieldError.scss";
 
-const FieldError = ({
-  children,
-}: {children?: string | undefined}) => (
-  <div className={`FieldError ${!children ? 'FieldError__hidden' : ''}`}>
-    {/* Render '\u00A0' (non-breaking space) to ensure we maintain spacing even when hidden */}
-    <div className="FieldError__message">{children || '\u00A0'}</div>
-  </div>
-);
+const nonBreakingSpace = "\u00A0";
+
+const FieldError = ({ children }: { children?: string }) => {
+  const [displayedMessage, setDisplayedMessage] = useState(
+    children || nonBreakingSpace
+  );
+  const [isVisible, setIsVisible] = useState(!!children);
+
+  useEffect(() => {
+    if (children) {
+      if (displayedMessage !== nonBreakingSpace && displayedMessage !== children) {
+        // Transitioning between error message states
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setIsVisible(false);
+        const timeout = setTimeout(() => {
+          setDisplayedMessage(children);
+          setIsVisible(true);
+        }, 100); // Half the transition time for a quick crossfade
+        return () => clearTimeout(timeout);
+      } else {
+        // New error or same error
+        setIsVisible(true);
+        setDisplayedMessage(children);
+      }
+    } else {
+      // Error cleared
+      setIsVisible(false);
+      const timeout = setTimeout(() => {
+        setDisplayedMessage(nonBreakingSpace);
+      // Setting timeout to same length as transition duration in scss
+      }, 200);
+      return () => clearTimeout(timeout);
+    }
+  }, [children, displayedMessage]);
+
+  return (
+    <div className={`FieldError ${!isVisible ? "FieldError--hidden" : ""}`}>
+      <div className="FieldError__message">{displayedMessage}</div>
+    </div>
+  );
+};
 
 export default FieldError;
